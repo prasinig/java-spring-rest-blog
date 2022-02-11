f7bb4cf3f2b47489ee6970792e4592ee981f04b8
P Singh <prabesh.singh@insightglobal.com>
2022-02-11 16:27:28 -0500

    Add Search by Title Containing

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @RestResource
 @Component
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    public List<Post> findByTitleContaining(String title);
 }
